bfcf1a398580327e0dba3b4507f8317f1adea399
astek <jeremy.brunel@bashroom.com>
2024-04-07 15:37:44 +0200

    feat: add favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+// pages/_document.tsx
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          {/* You can add other resources here like fonts, stylesheets etc. */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
